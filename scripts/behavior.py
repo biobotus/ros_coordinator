e8a0db1ecb4a5d78a2b11c872e75f6026b4b3868
Marc-Antoine Deragon <marc-a.deragon@usherbrooke.ca>
2016-03-12 13:16:28 -0500

    Behavior ROS topics updated

--- scripts/behavior.py
+++ scripts/behavior.py
@@ -2,7 +2,7 @@
 
 # imports
 import rospy
-from std_msgs.msg import Float32, String, Int32, Float32MultiArray, Int32MultiArray
+from std_msgs.msg import Float32, String, Int32, Float32MultiArray, Int32MultiArray, Bool
 
 # variables
 node_name = 'motor_control_y'
@@ -11,20 +11,30 @@ class behavior():
     def __init__(self):
         self.delta_x = 0
         self.delta_y = 0
-        self.delta_z = 0
+        self.delta_simplepip = 0
+		self.delta_multipip = 0
+		self.delta_gripper = 0
 
         self.actual_pos_x = 0
         self.actual_pos_y = 0
-        self.actual_pos_z = 0
+        self.actual_pos_simplepip = 0
+		self.actual_pos_multipip = 0
+		self.actual_pos_gripper = 0
 
         self.new_pos_x = 0
         self.new_pos_y = 0
-        self.new_pos_z = 0
-
-        self_pulse_x = 0
-        self_pulse_y = 0
-        self_pulse_z = 0
-        
+        self.new_pos_simplepip = 0
+		self.new_pos_multipip = 0
+		self.new_pos_gripper = 0
+		
+        self.pulse_x = 0
+        self.pulse_y = 0
+        self.pulse_simplepip = 0
+        self.pulse_multipip = 0
+		self.pulse_gripper = 0
+		
+		self.z_id = 0
+		
         # Constants
         self.dist_step = 0.127
         self.mode_step = 0.25 # 4 pulses per step
@@ -33,18 +43,22 @@ class behavior():
         # Error control
         self.err_pulse_x = 0
         self.err_pulse_y = 0
-        self.err_pulse_z = 0
+        self.err_pulse_simplepip = 0
+		self.err_pulse_multipip = 0
+		self.err_pulse_gripper = 0
 
         # ROS publishments
-        self.pub_pulse_x = rospy.Publisher('Pulse_x',Int32MultiArray, queue_size=10) 
-        self.pub_pulse_y = rospy.Publisher('Pulse_y',Int32MultiArray, queue_size=10)
-        self.pub_pulse_z = rospy.Publisher('Pulse_z',Int32MultiArray, queue_size=10)               
-        self.pub_platform_init = rospy.Publisher('platform_init',Int32,queue_size=10)        
-
+        self.pub_pulse_XY = rospy.Publisher('Pulse_XY',Int32MultiArray, queue_size=10) 
+        self.pub_pulse_Z = rospy.Publisher('Pulse_Z',Int32MultiArray, queue_size=10)               
+        self.pub_platform_init = rospy.Publisher('platform_Init',Int32,queue_size=10)        
+		self.motor_kill = rospy.Publisher('Motor_Kill', String, queue_size=10)
+		self.step_state = rospy.Publisher('Step_State', Bool, queue_size=10)
+		
         # ROS subscriptions 
-        self.subscriber = rospy.Subscriber('New_pos',Float32MultiArray, self.callback_pos)
-        self.subscriber = rospy.Subscriber('Motor_Kill',String, self.callback_kill)
+        self.subscriber = rospy.Subscriber('New_Pos',Float32MultiArray, self.callback_pos)
         self.subscriber = rospy.Subscriber('Error',String,self.callback_error)        
+		self.subscriber = rospy.Subscriber('Done_Move',String,self.callback_done_move)
+		
 
         # ROS init
         self.rate = rospy.Rate(10) # 10Hz
@@ -53,7 +67,9 @@ class behavior():
     def callback_pos(self,data):
         self.new_pos_x = data.data[0]
         self.new_pos_y = data.data[1]
-        self.new_pos_z = data.data[2]
+        self.new_pos_simplepip = data.data[2]
+		self.new_pos_multipip = data.data[2]
+		self.new_pos_gripper = data.data[2]
         self.behavior_output_pulse()
 
     # Error management
@@ -65,20 +81,28 @@ class behavior():
     def behavior_ouptut__pulse(self):
         self.delta_x = self.new_pos_x - self.actual_pos_x
         self.delta_y = self.new_pos_y - self.actual_pos_y
-        self.delta_z = self.new_pos_z - self.actual_pos_z
+        self.delta_simplepip = self.new_pos_simplepip - self.actual_pos_simplepip
+        self.delta_multipip = self.new_pos_multipip - self.actual_pos_multipip
+        self.delta_gripper = self.new_pos_gripper - self.actual_pos_gripper
 
         pulse_temp_x = self.delta_x / self.pulse 
         pulse_temp_y = self.delta_y / self.pulse
-        pulse_temp_z = self.delta_z / self.pulse
+        pulse_temp_simplepip = self.delta_simplepip / self.pulse
+		pulse_temp_multipip = self.delta_multipip / self.pulse
+		pulse_temp_gripper = self.delta_gripper / self.pulse
                 
         self.pulse_x = int(round(pulse_temp_x))
         self.pulse_y = int(round(pulse_temp_y))
-        self.pulse_z = int(round(pulse_temp_z))
+        self.pulse_simplepip = int(round(pulse_temp_simplepip))
+		self.pulse_multipip = int(round(pulse_temp_multipip))
+		self.pulse_gripper = int(round(pulse_temp_gripper))
 
         # Adjust values if error is greater than 1 pulse
         self.err_pulse_x += pulse_temp_x - self.pulse_x
         self.err_pulse_y += pulse_temp_y - self.pulse_y
-        self.err_pulse_z += pulse_temp_z - self.pulse_z
+        self.err_pulse_simplepip += pulse_temp_simplepip - self.pulse_simplepip
+		self.err_pulse_multipip += pulse_temp_multipip - self.pulse_multipip
+		self.err_pulse_gripper += pulse_temp_gripper - self.pulse_gripper
 
         if self.err_pulse_x < -1 :
             self.err_pulse_x += 1
@@ -94,21 +118,43 @@ class behavior():
             self.err_pulse_y -= 1
             self.pulse_y += 1
 
-        if self.err_pulse_z < -1 :
-            self.err_pulse_z += 1
-            self.pulse_z -= 1
-        elif self.err_pulse_z > 1 :
-            self.err_pulse_z -= 1
-            self.pulse_z += 1
+        if self.err_pulse_simplepip < -1 :
+            self.err_pulse_simplepip += 1
+            self.pulse_simplepip -= 1
+        elif self.err_pulse_simplepip > 1 :
+            self.err_pulse_simplepip -= 1
+            self.pulse_simplepip += 1
+			
+		if self.err_pulse_multipip < -1 :
+            self.err_pulse_multipip += 1
+            self.pulse_multipip -= 1
+        elif self.err_pulse_multipip > 1 :
+            self.err_pulse_multipip -= 1
+            self.pulse_multipip += 1
+			
+		if self.err_pulse_gripper < -1 :
+            self.err_pulse_gripper += 1
+            self.pulse_gripper -= 1
+        elif self.err_pulse_gripper > 1 :
+            self.err_pulse_gripper -= 1
+            self.pulse_gripper += 1
 
-        self.pub_pulse_x.publish(self.pulse_x)
-        self.pub_pulse_y.publish(self.pulse_y)
-        self.pub_pulse_z.publish(self.pulse_z)  
+			
+		#Publish number of pulse for all axis
+		Pulse_XY = [self.pulse_x, self.pulse_y] 
+        self.pub_pulse_XY.publish(Pulse_XY)
+		
+		if (self.pulse_simplepip):
+			self.z_id = 0
+		elif (self.pulse_multipip):
+			self.z_id = 1
+		elif (self.pulse_gripper):
+			self.z_id = 2
+		
+		z_tools = [self.pulse_simplepip,self.pulse_multipip,self.pulse_gripper]
+		Pulse_Z = [z_id,z_tools[id]]
+        self.pub_pulse_Z.publish(Pulse_Z)  
 
-    # callback to kill motor
-    def callback_kill(self,data):
-        if data.data == node_name:
-            rospy.signal_shutdown(node_name)
 
     def listener(self):
         while not rospy.is_shutdown():
