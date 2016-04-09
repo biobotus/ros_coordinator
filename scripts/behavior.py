a1f6901302f3a4902bde19c30e154e2f22775d17
marcaderagon <marc-antoine.deragon@usherbrooke.ca>
2016-04-09 18:55:25 -0400

    Multi_pip and gripper added

--- scripts/behavior.py
+++ scripts/behavior.py
@@ -21,14 +21,18 @@ class Behavior():
                                            self.callback_new_step)
         self.subscriber = rospy.Subscriber('New_Step_Step', Int32MultiArray, \
                                            self.callback_pos_step)    # Pour test
+        self.subscriber = rospy.Subscriber('New_Step_XY', Int32MultiArray, self.callback_XY_step)
+        self.subscriber = rospy.Subscriber('New_Step_Z', Int32MultiArray, self.callback_Z_step)
         self.subscriber = rospy.Subscriber('Error', String, self.callback_error)
         self.subscriber = rospy.Subscriber('Done_Move',String,self.callback_done_move)
 
         # ROS publishments
         self.motor_kill = rospy.Publisher('Motor_Kill', String, queue_size=10)
-        self.platform_init = rospy.Publisher('Platform_Init', Bool, queue_size=10)
+        self.platform_init = rospy.Publisher('Platform_Init', String, queue_size=10)
         self.pub_pulse_xy = rospy.Publisher('Pulse_XY', IntList, queue_size=10)
         self.pub_pulse_z = rospy.Publisher('Pulse_Z', IntList, queue_size=10)
+        self.pub_pulse_sp = rospy.Publisher('Pulse_SP', IntList, queue_size=10)
+        self.pub_gripper_pos = rospy.Publisher('Gripper_Pos', string, queue_size=10)
         self.step_done = rospy.Publisher('Step_Done', Bool, queue_size=10)
 
         
@@ -40,6 +44,8 @@ class Behavior():
         self.actual_pos_x = 0.0
         self.actual_pos_y = 0.0
         self.actual_pos_z = [0.0, 0.0, 0.0]
+        self.actual_pos_sp = 0
+        self.acutla_pos_mp = 0
         
         self.new_pos_x = 0.0
         self.new_pos_y = 0.0
@@ -48,16 +54,29 @@ class Behavior():
         self.pulse_x = 0
         self.pulse_y = 0
         self.pulse_z = [0, 0, 0]
+        self.pulse_sp = 0
+        self.pulse_mp = 0
 
         # Constants
         self.dist_step_xy = 0.127
         self.mode_step_xy = 0.25  # 4 pulses per step
         self.pulse_cst_xy = self.dist_step_xy * self.mode_step_xy
         
-        self.dist_step_z = 0.127
+        self.dist_step_z = 0.04
         self.mode_step_z = 0.25  # 4 pulses per step
         self.pulse_cst_z = self.dist_step_z * self.mode_step_z
         
+        self.pip_lim = 70000  # number of pulse limits
+        
+        # pipette tips equations variables
+        self.pip_slope_tip20 = 97.61071086
+        self.pip_intercept_tip20 = 63.73267033
+        
+        self.pip_slope_tip200 = 99.91081613
+        self.pip_intercept_tip200 = 68.43652967
+
+        self.pip_slope_tip1000 = 97.84199255
+        self.pip_intercept_tip1000 = 328.9959479        
         
         # Error control
         self.err_pulse_x = 0
@@ -78,52 +97,181 @@ class Behavior():
 
         except (AssertionError, AttributeError) as e:
             print("Erreur : {}".format(e))
-            return -1
+            return None
             
     # Callback position for step
     def callback_pos_step(self,data):
         step_x = data.data[0]
         step_y = data.data[1]
+        step_z = data.data[2]
 
 
         #Publish number of pulse for all axis
         pulse_XY = IntList()
         pulse_XY.data = [step_x, step_y]
         print(pulse_XY.data)
+        
         self.pub_pulse_xy.publish(pulse_XY)
+        
+        pulse_Z = IntList()
+        pulse_Z.data = [0, step_z]
+        print(pulse_Z.data)
+        
+        self.pub_pulse_z.publish(pulse_Z)
 
+    # Callback position for step
+    def callback_XY_step(self,data):
+        step_x = data.data[0]
+        step_y = data.data[1]
+
+        #Publish number of pulse for all axis
+        pulse_XY = IntList()
+        pulse_XY.data = [step_x, step_y]
+        print(pulse_XY.data)
+        
+        self.pub_pulse_xy.publish(pulse_XY)
+        
+    # Callback position for step
+    def callback_Z_step(self,data):
+        z_id_test = data.data[0]
+        step_z = data.data[1]
+
+        pulse_Z = IntList()
+        pulse_Z.data = [z_id_test, step_z]
+        print(pulse_Z.data)
+        
+        self.pub_pulse_z.publish(pulse_Z)
+
+        
     def callback_done_move(self, data):
         
         try:
             assert data.data in self.done_move
-            self.done_move.remove(data.data)
             
-            if not self.done_move:
-                self.actual_pos_x = self.new_pos_x
-                self.actual_pos_y = self.new_pos_y
-                self.actual_pos_z = self.new_pos_z
-            
-                print(self.actual_pos_x)
-                print(self.actual_pos_y)
-                print(self.actual_pos_z)
-            
-                self.step_done.publish(True)
-                
         except (AssertionError, AttributeError) as e:
             print("Error : wrong done_move received: {}".format(e))
-            return -1
+            return None
+            
+        self.done_move.remove(data.data)
+        
+        if not self.done_move:
+            self.actual_pos_x = self.new_pos_x
+            self.actual_pos_y = self.new_pos_y
+            self.actual_pos_z = self.new_pos_z
+            self.actual_pos_sp = self.pulse_sp + self.actual_pos_sp
+            self.actual_pos_mp = self.pulse_mp + self.actual_pos_mp
+            self.pulse_mp = 0 
+            self.pulse_sp = 0
+            self.step_done.publish(True)
+                
+        
         
     # Error management
     def callback_error(self,data):
-        self.platform_init.publish(True)
+        pass
+        #self.platform_init.publish(True)
 
+    # Publish pipette_s topics in function of self.step_dict args
     def send_pipette_s(self):
         if self.step_dict['params']['name'] == 'pos':
             return self.send_pos(0)
-        #manip still to implement
-        return 
         
+        try:
+            assert self.step_dict['params']['name'] == 'manip'
         
+        except (AssertionError, AtributeError) as e:
+            print("Error with params name in dict: {}".format(e))
+            return None
+            
+        
+        vol = self.step_dict['params']['args']['vol']
+        
+        if abs(vol) < 10 :
+            self.pulse_sp = self.pip_slope_tip20*vol + self.pip_intercept_tip20
+        elif (abs(vol) >= 10) and (abs(vol) < 100):
+            self.pulse_sp = self.pip_slope_tip200*vol + self.pip_intercept_tip200
+        elif (abs(vol) > 100) and (abs(vol) < 800):
+            self.pulse_sp = self.pip_slope_tip1000*vol + self.pip_intercept_tip1000
+        else :
+            print("Error wrong volume entered")
+            return None
+
+        try :
+            assert (self.pulse_sp + self.actual_pos_sp) > self.pip_lim
+            assert (self.pulse_sp + self.actual_pos_sp) < 0
+            
+        except (AssertionError, AtributeError) as e:
+            print("Impossible SP manip, volume out of range: {}".format(e))
+            return None
+            
+        freq_sp = round((self.step_dict['params']['args']['speed'] * self.pulse_sp) / vol)
+        
+        #Publish number of pulse for simple pip
+        pulse_SP = IntList()
+        pulse_SP.data = [freq_sp, self.pulse_sp]
+        print("pulse_sp : {}".format(pulse_SP.data))
+        self.pub_pulse_sp.publish(pulse_SP)
+        self.done_move = ['MotorControlSP']
+        
+    # Publish pipette_mp topics in function of self.step_dict args
+    def send_pipette_mp(self):
+        if self.step_dict['params']['name'] == 'pos':
+            return self.send_pos(1)
+        
+        try:
+            assert self.step_dict['params']['name'] == 'manip'
+        
+        except (AssertionError, AtributeError) as e:
+            print("Error with params name in dict: {}".format(e))
+            return None
+            
+        
+        vol = self.step_dict['params']['args']['vol']
+        
+        if abs(vol) < 10 :
+            self.pulse_mp = self.pip_slope_tip20*vol + self.pip_intercept_tip20
+        elif (abs(vol) >= 10) and (abs(vol) < 100):
+            self.pulse_mp = self.pip_slope_tip200*vol + self.pip_intercept_tip200
+        elif (abs(vol) > 100) and (abs(vol) < 800):
+            self.pulse_mp = self.pip_slope_tip1000*vol + self.pip_intercept_tip1000
+        else :
+            print("Error wrong volume entered")
+            return None
+
+        try :
+            assert (self.pulse_mp + self.actual_pos_mp) > self.pip_lim
+            assert (self.pulse_mp + self.actual_pos_mp) < 0
+            
+        except (AssertionError, AtributeError) as e:
+            print("Impossible MP manip, volume out of range: {}".format(e))
+            return None
+            
+        freq_mp = round((self.step_dict['params']['args']['speed'] * self.pulse_mp) / vol)
+        self.pulse_mp = round(self.pulse_mp)
+        #Publish number of pulse for simple pip
+        pulse_MP = IntList()
+        pulse_MP.data = [freq_mp, self.pulse_mp]
+        print("pulse_mp : {}".format(pulse_MP.data))
+        self.pub_pulse_mp.publish(pulse_MP)
+        self.done_move = ['MotorControlMP']
+        
+    # Publish gripper topics in function of self.step_dict args
+    def send_gripper(self):
+        if self.step_dict['params']['name'] == 'pos':
+            return self.send_pos(2)
+        
+        try:
+            assert self.step_dict['params']['name'] == 'manip'
+        
+        except (AssertionError, AtributeError) as e:
+            print("Error with params name in dict: {}".format(e))
+            return None
+            
+        gripper = self.step_dict['params']['args']
+        self.pub_gripper_pos.publish(gripper)
+        self.done_move = ['Gripper']
+        
+    
     # Compute and publish the number of pulse for each axes
     def send_pos(self, z_id):
 
